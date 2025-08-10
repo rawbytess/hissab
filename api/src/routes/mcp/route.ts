af58ad23f1e9776f060cbcf15d5d552751f43c1a
Mufaddal Makati <mufaddal@rawbytes.com>
2025-08-10 16:35:41 -0400

    mcp log fix

--- api/src/routes/mcp/route.ts
+++ api/src/routes/mcp/route.ts
@@ -1,4 +1,5 @@
 import { StreamableHTTPTransport } from "@hono/mcp";
+import { cloudflareRateLimiter } from "@hono-rate-limiter/cloudflare";
 import documentation from "@lib/ai/instructions/documentation";
 import { mcpInstructions } from "@lib/ai/instructions/system-instructions";
 import type { MetaBindings } from "@lib/types/envTypes";
@@ -6,31 +7,12 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { Hono } from "hono";
 import { z } from "zod";
 import { calculateExpressions } from "~lib/calculateExpressions";
-import { cloudflareRateLimiter } from "@hono-rate-limiter/cloudflare";
 import { safeGet } from "~lib/utils";
 
 const server = new McpServer({
   name: "hissab-mcp",
   version: "0.1.0",
 });
-
-server.registerTool(
-  "Hissab",
-  {
-    title: "Hissab Math & calculation Tool",
-    description: mcpInstructions + documentation,
-    inputSchema: { hissab_expressions: z.array(z.string()) },
-  },
-  async ({ hissab_expressions }) => {
-    const results = await calculateExpressions(hissab_expressions, true);
-    const formattedResults = results
-      .map((item) => `${item.expression} = ${item.result}`)
-      .join("\n");
-    return {
-      content: [{ type: "text", text: formattedResults }],
-    };
-  },
-);
 
 const app = new Hono<MetaBindings>();
 app.use(
@@ -44,7 +26,27 @@ app.all("/", async (c) => {
   const body = await c.req.text();
   const db = c.env.LOGS_DB;
   const transport = new StreamableHTTPTransport();
+  server.registerTool(
+    "Hissab",
+    {
+      title: "Hissab Math & calculation Tool",
+      description: mcpInstructions + documentation,
+      inputSchema: { hissab_expressions: z.array(z.string()) },
+    },
+    async ({ hissab_expressions }) => {
+      c.executionCtx.waitUntil(logdata(db, JSON.stringify(hissab_expressions)));
+      const results = await calculateExpressions(hissab_expressions, true);
+      const formattedResults = results
+        .map((item) => `${item.expression} = ${item.result}`)
+        .join("\n");
+      return {
+        content: [{ type: "text", text: formattedResults }],
+      };
+    },
+  );
+
   await server.connect(transport);
+
   c.executionCtx.waitUntil(logdata(db, body));
   return transport.handleRequest(c);
 });
@@ -52,7 +54,7 @@ app.all("/", async (c) => {
 async function logdata(db: D1Database, data: string) {
   try {
     const jsonData = JSON.parse(data);
-    const method = jsonData.method || "unknown";
+    const method = jsonData?.method || "unknown";
     const client = safeGet(jsonData, "params.clientInfo.name");
     await db
       .prepare(
@@ -61,7 +63,9 @@ async function logdata(db: D1Database, data: string) {
       )
       .bind(data, method, client)
       .run();
-  } catch (error) {}
+  } catch (error: any) {
+    console.error(error?.message || error);
+  }
 }
 
 export default app;
